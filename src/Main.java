59c5585ce3f07eedd9b375d3d24e6f7da5f65daa
zemscky <aleksei.zemscky@yandex.ru>
2022-09-29 17:08:45 +0300

    Домашняя работа 1. Задание 1. String Изменения

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
         String firstName = "Ivanov";
         String middleName = "Ivan";
         String lastName = "Ivanovich";
-        String fullName = "Ivanov Ivan Ivanovich";
+        String fullName = "Ivanov" + " " + "Ivan" + " " + "Ivanovich";
         System.out.println("ФИО сотрудника — " + firstName +" "+ middleName +" "+ lastName); //1
         System.out.println();
         System.out.println("ФИО сотрудника — " + fullName); //2
