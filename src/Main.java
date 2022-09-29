06f1121e1485d26b817279d264a3895bdd48dc78
zemscky <aleksei.zemscky@yandex.ru>
2022-09-29 18:51:37 +0300

    Домашняя работа 1. Задание 3. String

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         taskOne ();
         taskTwo ();
+        taskThree ();
     }
     private static void taskOne () {
         System.out.println("Задание 1");
@@ -18,8 +19,22 @@ public class Main {
     }
     private static void taskTwo () {
         System.out.println("Задание 2");
+
         String fullName = "Ivanov Ivan Ivanovich";
         String upperСaseName = fullName.toUpperCase();
         System.out.println(upperСaseName);
+    }
+    private static void taskThree () {
+        System.out.println("Задание 3");
+
+        String fullName = "Иванов Семён Семёнович";
+        boolean notAcceptedCharacters = fullName.contains("ё"); // Проверка есть ли буква "ё"
+        System.out.println(notAcceptedCharacters);
+        System.out.println();
+        String[] fullNameSplit = fullName.split("\\s+");// разделение строки с помощью метода split()
+        for (String finishedFullName : fullNameSplit) {
+            String changeName = finishedFullName.replace('ё', 'е'); // Замена буквы "ё" на букву "е"
+            System.out.println(changeName);
+        }
     }
 }
