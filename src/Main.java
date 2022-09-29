e586c0a4a52190d45b7fac55826e2767b9e0a388
zemscky <aleksei.zemscky@yandex.ru>
2022-09-29 17:04:18 +0300

    Домашняя работа 1. Задание 1. String

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
+        taskOne ();
+    }
+    public static void taskOne () {
+        System.out.println("Задание 1");
+
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("ФИО сотрудника — " + firstName +" "+ middleName +" "+ lastName); //1
+        System.out.println();
+        System.out.println("ФИО сотрудника — " + fullName); //2
+
 
     }
 }
